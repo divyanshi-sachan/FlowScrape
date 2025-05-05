8c1de2350b47e0a7e3f7be7109c2ec4ea6e6d116
Divyanshi sachan <sachandivyanshi376@gmail.com>
2025-05-05 14:40:35 +0530

    mdsm

--- app/api/workflows/cron/route.ts
+++ app/api/workflows/cron/route.ts
@@ -2,20 +2,27 @@ import { getAppUrl } from "@/lib/helper/url";
 import prisma from "@/lib/prisma";
 import { WorkflowStatus } from "@/types/workflow";
 export async function GET(req: Request) {
-    const now = new Date();
-    const workflows = await prisma.workflow.findMany({
-        select: { id: true },
-        where: {
-            status: WorkflowStatus.PUBLISHED,
-            cron: { not: null },
-            nextRunAt: { lte: now },
-        }
-    });
-      for (const workflow of workflows) {
-        triggerWorkflow(workflow.id);
-      }
-    return Response.json({workflowsToRun:workflows.length},{status:200});
+  if (process.env.VERCEL && process.env.NEXT_PHASE === "phase-production-build") {
+    return new Response("Build-time call blocked", { status: 403 });
+  }
+
+  const now = new Date();
+  const workflows = await prisma.workflow.findMany({
+    select: { id: true },
+    where: {
+      status: WorkflowStatus.PUBLISHED,
+      cron: { not: null },
+      nextRunAt: { lte: now },
+    }
+  });
+
+  for (const workflow of workflows) {
+    await triggerWorkflow(workflow.id);
+  }
+
+  return Response.json({ workflowsToRun: workflows.length }, { status: 200 });
 }
+
 
 async function  triggerWorkflow(workflowId: string) {
     const triggerApiUrl = getAppUrl(`api/workflows/execute?workflowId=${workflowId}`);
